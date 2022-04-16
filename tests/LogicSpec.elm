0c9777a48dd946f1456568171030043148eef576
Andi Miller <andi@andimiller.net>
2022-04-16 22:45:10 +0000

    condense tests

--- tests/LogicSpec.elm
+++ tests/LogicSpec.elm
@@ -12,39 +12,19 @@ suite =
         [ describe "markInput"
             [ test "should mark correct input as correct" <|
                 \_ ->
-                    let
-                        result =
-                            markInput "hello" "hello"
-                    in
-                    Expect.equal result [ ( 'h', Correct ), ( 'e', Correct ), ( 'l', Correct ), ( 'l', Correct ), ( 'o', Correct ) ]
+                    Expect.equal (markInput "hello" "hello") [ ( 'h', Correct ), ( 'e', Correct ), ( 'l', Correct ), ( 'l', Correct ), ( 'o', Correct ) ]
             , test "should mark incorrect input as incorrect" <|
                 \_ ->
-                    let
-                        result =
-                            markInput "aaa" "bbb"
-                    in
-                    Expect.equal result [ ( 'b', Incorrect ), ( 'b', Incorrect ), ( 'b', Incorrect ) ]
+                    Expect.equal (markInput "aaa" "bbb") [ ( 'b', Incorrect ), ( 'b', Incorrect ), ( 'b', Incorrect ) ]
             , test "should detect wrongly placed characters" <|
                 \_ ->
-                    let
-                        result =
-                            markInput "ab" "ba"
-                    in
-                    Expect.equal result [ ( 'b', Elsewhere ), ( 'a', Elsewhere ) ]
+                    Expect.equal (markInput "ab" "ba")[ ( 'b', Elsewhere ), ( 'a', Elsewhere ) ]
             , test "should detect wrongly placed characters but not repeated ones" <|
                 \_ ->
-                    let
-                        result =
-                            markInput "ab" "bb"
-                    in
-                    Expect.equal result [ ( 'b', Incorrect ), ( 'b', Correct ) ]
+                    Expect.equal (markInput "ab" "bb")[ ( 'b', Incorrect ), ( 'b', Correct ) ]
             , test "should detect wrongly placed characters but not repeated ones 2" <|
                 \_ ->
-                    let
-                        result =
-                            markInput "abb" "bba"
-                    in
-                    Expect.equal result [ ( 'b', Elsewhere ), ( 'b', Correct ), ( 'a', Elsewhere ) ]
+                    Expect.equal (markInput "abb" "bba") [ ( 'b', Elsewhere ), ( 'b', Correct ), ( 'a', Elsewhere ) ]
             ]
         , describe "hasWon"
             [ test "says they've won if all are correct" <|
